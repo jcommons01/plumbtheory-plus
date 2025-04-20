c7e416c1acba88f5b66fa4f044b62c0ddbb597a7
Jordon Commons <jordcommons@gmail.com>
2025-04-20 10:14:50 +0100

    Initial commit - PlumbTheory+

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,31 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '@/contexts/AuthProvider';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      <Head>
+        {/* Google Ads Global Site Tag */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17021468689"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17021468689');
+            `,
+          }}
+        />
+      </Head>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
   );
 }
